0033a3012103ca03041e70a4ed90f3ec1907a0a5
Gabriela Veloz <velozitywc@gmail.com>
2020-05-19 23:49:50 -0600

    front end edits

--- app/src/pages/SignUp.js
+++ app/src/pages/SignUp.js
@@ -1,11 +1,21 @@
 import React from "react"
 import {Form} from "react-bootstrap"
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Signup from "../components/css/signup.css"
+
+
 
 export const SignUp = () => {
 	return (
 		<>
 			<h1>Sign Up</h1>
+			<Jumbotron fluid>
+					<h1>Date Night</h1>
+			</Jumbotron>
+
+			<div>
 			<Form>
+				<h3>Sign up now to start your adventure!</h3>
 				<Form.Group controlId="formGroupEmail">
 					<Form.Label>Email address</Form.Label>
 					<Form.Control type="email" placeholder="Enter email" />
@@ -19,6 +29,7 @@ export const SignUp = () => {
 					<Form.Control type="password" placeholder="Password" />
 				</Form.Group>
 			</Form>
+			</div>
 
 		</>
 	)
